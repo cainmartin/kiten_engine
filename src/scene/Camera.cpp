61335998a4646bc1e7ebf04af6fd5f712036e37e
cainmartin <cain.martin@gmail.com>
2024-12-10 00:45:41 +0900

    Camera class

--- src/scene/Camera.cpp
+++ src/scene/Camera.cpp
@@ -3,3 +3,79 @@
 //
 
 #include "Camera.h"
+
+
+Camera::Camera(const glm::vec3& position, float pitch, float yaw, float fov, float aspect, float near, float far)
+: m_position(position)
+, m_pitch(pitch)
+, m_yaw(yaw)
+, m_fov(fov)
+, m_ratio(aspect)
+, m_near(near)
+, m_far(far)
+{
+    m_up = glm::vec3(0.0, 1.0, 0.0);
+    update_camera_vectors();
+    update();
+}
+
+void Camera::set_position(const glm::vec3& position)
+{
+    m_position = position;
+    update();
+}
+
+void Camera::set_look_at(const glm::vec3& target)
+{
+    m_forward = glm::normalize(target - m_position);
+    m_right = glm::normalize(glm::cross(m_forward, glm::vec3(0.0, 1.0, 0.0)));
+    m_up = glm::normalize(glm::cross(m_right, m_forward));
+    update();
+}
+
+void Camera::move_forward(float delta)
+{
+    m_position += m_forward * delta;
+    update();
+}
+
+void Camera::move_up(float delta)
+{
+    m_position += m_up * delta;
+    update();
+}
+
+void Camera::move_right(float delta)
+{
+    m_position += m_right * delta;
+    update();
+}
+
+void Camera::update()
+{
+    m_view_matrix = glm::lookAt(m_position, m_position + m_forward, m_up);
+    m_projection_matrix = glm::perspective(glm::radians(m_fov), m_ratio, m_near, m_far);
+}
+
+void Camera::update_camera_vectors()
+{
+    glm::vec3 forward;
+    forward.x = cos(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
+    forward.y = sin(glm::radians(m_pitch));
+    forward.z = sin(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
+
+    m_forward = glm::normalize(forward);
+
+    m_right = glm::normalize(glm::cross(m_forward, glm::vec3(0.0, 1.0, 0.0)));
+    m_up = glm::normalize(glm::cross(m_right, m_forward));
+}
+
+glm::mat4 Camera::get_view_matrix() const
+{
+    return m_view_matrix;
+}
+
+glm::mat4 Camera::get_projection_matrix() const
+{
+    return m_projection_matrix;
+}
--- src/scene/Camera.h
+++ src/scene/Camera.h
@@ -12,9 +12,32 @@
 class Camera
 {
 public:
+    Camera(const glm::vec3& position, float pitch, float yaw, float fov, float aspect, float near, float far);
+    void set_position(const glm::vec3& position);
+    void set_look_at(const glm::vec3& target);
+    void move_forward(float delta);
+    void move_up(float delta);
+    void move_right(float delta);
+    void update();
+    glm::mat4 get_view_matrix() const;
+    glm::mat4 get_projection_matrix() const;
 
 private:
+    glm::vec3 m_position;
+    glm::vec3 m_forward;
+    glm::vec3 m_right;
+    glm::vec3 m_up;
+    float m_fov;
+    float m_ratio;
+    float m_pitch;
+    float m_yaw;
+    float m_near;
+    float m_far;
 
+    glm::mat4 m_view_matrix;
+    glm::mat4 m_projection_matrix;
+
+    void update_camera_vectors();
 };
 
 
